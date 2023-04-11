7a9ce3f81813131f12891a2f7eb37dba3a558c89
Julian Shniter <julianshniter@Julians-MacBook-Pro.local>
2023-04-11 16:13:00 -0500

    pull demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+/* export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -120,5 +120,14 @@ export default function Home() {
         </a>
       </div>
     </main>
+  )
+}
+ */
+
+export default function Home () {
+  return (
+    <div>
+      <h1>Chris is a homosexual</h1>
+    </div>
   )
 }
